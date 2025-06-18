52e25e5ef67eef0b8adc9f0da4d1b381f51fd5db
NicoMaximiliano <nicolas.orellano25@outlook.com.ar>
2025-06-17 21:24:07 -0300

    Borrado de importaciones que no se usaran

--- src/main/java/org/nicode/api_vuelos/web/controllers/AirportDestinyController.java
+++ src/main/java/org/nicode/api_vuelos/web/controllers/AirportDestinyController.java
@@ -5,7 +5,6 @@ import org.nicode.api_vuelos.domain.dtos.AirportDestiny;
 import org.nicode.api_vuelos.domain.dtos.responses.SuccessfulResponse;
 import org.nicode.api_vuelos.domain.services.IAirportDestinyService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
